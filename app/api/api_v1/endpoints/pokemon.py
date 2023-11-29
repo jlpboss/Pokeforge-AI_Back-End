8d078422ae85798f158056faa441a28c0721d869
Jett Pennington <jettpennington@gmail.com>
2023-11-29 15:39:31 +0000

    added all in tiars route

---
+++ app/api/api_v1/endpoints/pokemon.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends
+import requests
+from pokeforge.find_all_tiers import find_all_tiers
+
+router = APIRouter()
+
+api_url = "https://play.pokemonshowdown.com/data/pokedex.json"
+response = requests.get(api_url)
+if response.status_code == 200:
+    response = response.json()
+else:
+    response = []
+
+@router.get("/all-tiers")
+def get_all_tiers():
+    return find_all_tiers(response)
